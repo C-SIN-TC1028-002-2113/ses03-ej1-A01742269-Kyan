e6b1fc35f3ad7f4919b68d2e20b19126d65e9b51
A01742269-Kyan <A01742269@itesm.mx>
2021-08-17 14:44:18 +0000

    CantidadLitrosPintura

--- assignments/13CantidadLitrosPintura/src/exercise.py
+++ assignments/13CantidadLitrosPintura/src/exercise.py
@@ -4,7 +4,7 @@ def main():
     area = float(input("Area a pintar en metros: "))
     rendimiento = float(input("Rendimiento (m2/1): "))
     l = math.ceil(rendimiento/area)
-    print("Litros a comprar: ",l)
+    print("Litros a comprar:",l)
 
 if __name__ == '__main__':
     main()
